7cd958d3725ebbae22362e64dca9ad688e8785d1
Ji-Hong-PARK <113972972+Ji-Hong-PARK@users.noreply.github.com>
2022-10-06 12:08:44 +0900

    박지홍-2022-10-06

--- src/PARKJIHONG/PARKJIHONG.java
+++ src/PARKJIHONG/PARKJIHONG.java
@@ -6,6 +6,7 @@ public class PARKJIHONG {
 		// TODO Auto-generated method stub
 		System.out.println("선생님 오늘도 고생 많으셨습니다! ");
 		System.out.println("시험 제출하고 남은 시간 자습시간동안 JAVA 예습하도록 하겠습니다.");
+		System.out.println("Hello World");
 	}
 
 }
