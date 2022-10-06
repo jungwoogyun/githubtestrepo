b30d44f3357ff9f0b3640a1ad28533958009926c
syrah97 <dcyc8326@naver.com>
2022-10-06 12:10:03 +0900

    221006내부평가최용채

--- src/최용채/최용채.java
+++ src/최용채/최용채.java
@@ -5,6 +5,8 @@ public class 최용채 {
 	public static void main(String[] args) {
 		
 		System.out.println("안녕하세요 최용채입니다.");
+		System.out.println("내부평가!!");
+	
 		// TODO Auto-generated method stub
 
 	}
